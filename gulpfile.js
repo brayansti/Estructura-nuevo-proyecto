d987c78585577fb38435e89b3e182e231ef86966
Brayan Camargo <brayan@150porciento.com>
2017-03-23 09:06:36 -0500

    Actualizacion estructura

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,78 @@
-'use strict';
-var gulp = require('gulp');
-var server = require('gulp-server-livereload');
-var pug = require('gulp-pug');
+var rutaCSS = 'deploy/css/'; // ruta del CSS final
+var rutaJS = 'deploy/js/'; // ruta del archivo JS final
+var rutaIMG = 'deploy/img/'; //carpeta donde compila las imágenes
+var nameJS = 'mainscript.js'; //nombre final de los archivos concatenados
+
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('webserver', function() {
-  gulp.src('')
-    .pipe(server({
-      livereload: true,
-      directoryListing: true,
-      port: 8000,
-      open: true
-    }));
+// gulp.task('images', function(){
+//   gulp.src('sources/images/*')
+//     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+//     .pipe(gulp.dest(rutaIMG));
+// });
+gulp.task('images', () =>
+    gulp.src('sources/images/*/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(rutaIMG))
+);
+
+gulp.task('styles', function(){
+  gulp.src(['sources/sass/main.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(rutaCSS))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss({keepSpecialComments: 1, processImport: false}))
+    .pipe(gulp.dest(rutaCSS))
 });
 
-
-gulp.task('sass', function () {
-  return gulp.src('src/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dest/css/'));
+gulp.task('scripts', function(){
+  return gulp.src('sources/js/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(concat(nameJS))
+    .pipe(gulp.dest(rutaJS))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest(rutaJS))
 });
-
-
-gulp.task('templates', function() {
-  var YOUR_LOCALS = {};
-
-  gulp.src('src/pug/*.pug')
-    .pipe(pug({
-      locals: YOUR_LOCALS
-    }))
-    .pipe(gulp.dest('dest/'))
-});
-
 
 gulp.task('watch', function() {
-  gulp.watch('src/pug/*', ['templates']);
-  gulp.watch('src/sass/*', ['sass'])
+  //gulp.watch('jade/*.jade',['templates']);
+  //gulp.watch('jade/includes/*.jade',['templates']);
+  // ↓↓↓ Sass watch
+  gulp.watch('sources/sass/**/*.scss', ['styles']);
+  gulp.watch('sources/js/*.js', ['scripts']);
+  // gulp.watch('sources/images/*/*', ['images']);
+  //gulp.watch('js/*.js', ['compress']);
+  // ↑↑↑ Sass watch
 });
 
-gulp.task('default', ['webserver', 'sass', 'templates', 'watch']);
+//gulp.task('default', function(){
+//  gulp.watch("sources/sass/**/*.scss", ['styles']);
+//  gulp.watch("sources/js/**/*.js", ['scripts']);
+//});
+
+// tareas default
+gulp.task('default', ['styles' , 'scripts' , 'watch']);
